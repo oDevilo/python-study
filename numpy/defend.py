fe8749722c17228512459d0e645bcdd9119b428b
yuansheng <yuansheng@raycloud.com>
2019-01-05 22:18:02 +0800

    numpy

---
+++ numpy/defend.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+x = np.linspace(0, 100,100)
+# for y in range(1, 51):
+#     z = x / (x + 300 + 50 * y)
+#     plt.plot(x,  z, label = y)
+# plt.legend()
+# plt.show()
+y = x / (x + 1000)
+plt.plot(x,  y)
+#z = np.power(x,0.3) / 100
+#plt.plot(x,  z)
+#plt.legend()
+plt.show()
